f1efbe2735ab80bc3f5b124082c422bf8ed799f6
Vansh <vanshchoudhary2744@gmail.com>
2026-09-14 20:31:27 +0530

    another examples of type assestion

--- dist/typeAssertion.js
+++ dist/typeAssertion.js
@@ -1,7 +1,27 @@
 "use strict";
+// const name: any = "Vansh";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Vansh";
-const len = name.length;
-const user = '{"name":"Vansh","email":"vansh@gmail.com","age":23}';
-console.log(JSON.parse(user).email);
+// const len = (name as string).length;
+// type User = {
+//   name: string;
+//   email: string;
+//   age: number;
+// };
+// const user = '{"name":"Vansh","email":"vansh@gmail.com","age":23}';
+// console.log((JSON.parse(user) as User).email);
+//-----------------------------------------
+function getUsers() {
+    throw new Error("Unable to fetch users");
+}
+try {
+    getUsers();
+}
+catch (error) {
+    if (error instanceof Error) {
+        console.log(error.message);
+    }
+    else {
+        console.log("Error", error);
+    }
+}
 //# sourceMappingURL=typeAssertion.js.map
--- src/typeAssertion.ts
+++ src/typeAssertion.ts
@@ -1,13 +1,37 @@
-const name: any = "Vansh";
+// const name: any = "Vansh";
 
-const len = (name as string).length;
+// const len = (name as string).length;
 
-type User = {
-  name: string;
-  email: string;
-  age: number;
-};
+// type User = {
+//   name: string;
+//   email: string;
+//   age: number;
+// };
 
-const user = '{"name":"Vansh","email":"vansh@gmail.com","age":23}';
+// const user = '{"name":"Vansh","email":"vansh@gmail.com","age":23}';
 
-console.log((JSON.parse(user) as User).email);
+// console.log((JSON.parse(user) as User).email);
+
+//-----------------------------------------
+
+// function getUsers() {
+//   throw new Error("Unable to fetch users");
+// }
+
+// try {
+//   getUsers();
+// } catch (error) {
+//   if (error instanceof Error) {
+//     console.log(error.message);
+//   } else {
+//     console.log("Error", error);
+//   }
+// }
+
+//-------------------------------------------
+
+// const name: unknown = "Vansh";
+
+// const newName: string = name as string;
+
+// console.log(newName);
