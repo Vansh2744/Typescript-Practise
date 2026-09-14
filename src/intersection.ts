f76c75d5e3e98839a723485fc9d6c63449921b3f
Vansh <vanshchoudhary2744@gmail.com>
2026-09-14 22:22:55 +0530

    intersection

---
+++ src/intersection.ts
@@ -0,0 +1,18 @@
+type User = { name: string; email: string };
+type Admin = { id: string; age: number };
+
+type Combine = User & Admin;
+
+// function displayInfo(user:Combine){
+
+// }
+
+// function displayInfo(user:User){
+
+// }
+
+// function displayInfo(user:Admin){
+
+// }
+
+// function displayInfo(user: User & Admin) {}
