e0617c9d3604a568e5c419e214a8441673b64c81
Vansh <vanshchoudhary2744@gmail.com>
2026-09-14 11:34:06 +0530

    type narrowing

---
+++ src/typeNarrowing.ts
@@ -0,0 +1,44 @@
+function displayUser(name: string, phone: string | number) {
+  if (typeof phone === "string") {
+    console.log(`Calling String type phone : ${name}`);
+  } else {
+    console.log(`Calling Number type phone : ${name}`);
+  }
+}
+
+let access: string | undefined;
+
+function dashboard(role: "USER" | "ADMIN" | "MEMBER") {
+  if (role === "ADMIN") {
+    access = "Full Access";
+  }
+  return `You Can Access all ${role} Features`;
+}
+
+class Auth {
+  validateUser() {
+    console.log("Validating Auth User...");
+  }
+}
+
+class Payment {
+  validateUser() {
+    console.log("Validating Payment User...");
+  }
+}
+
+function validate(method: Auth | Payment) {
+  if (method instanceof Auth) {
+    console.log("Authenticating User...");
+    method.validateUser();
+  } else {
+    console.log("Making Payment...");
+    method.validateUser();
+  }
+}
+
+const auth = new Auth();
+const pay = new Payment();
+
+validate(auth);
+validate(pay);
