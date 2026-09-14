d49aa9b2c8d5002d8c21e8fe9bf69606de94fe9f
Vansh <vanshchoudhary2744@gmail.com>
2026-09-15 00:47:06 +0530

    readonly

--- dist/default.js
+++ dist/default.js
@@ -1,8 +1,12 @@
 "use strict";
+// type User = {
+//   name: string;
+//   email: string;
+//   age?: number;
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
 function displayInfo(user) {
     return user;
 }
-console.log(displayInfo({ name: "Vansh", email: "vansh@gmail.com" }));
-console.log(displayInfo({ name: "Vansh", email: "vansh@gmail.com", age: 23 }));
+console.log(displayInfo({ id: "uehr8ty85", name: "Vansh", email: "vansh@gmail.com" }));
 //# sourceMappingURL=default.js.map
--- src/default.ts
+++ src/default.ts
@@ -12,3 +12,17 @@
 // console.log(displayInfo({ name: "Vansh", email: "vansh@gmail.com", age: 23 }));
 
 //--------------Readonly------------------
+
+type User = {
+  readonly id: string;
+  name: string;
+  email: string;
+};
+
+const user: User = {
+  id: "84uruhr84u8y8r884",
+  name: "Vansh",
+  email: "vansh@gmail.com",
+};
+
+user.id = "iur9tu4895u845"; // Gives error bcz id is read only
