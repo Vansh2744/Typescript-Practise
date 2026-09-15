e34fb41dfda5af973adae5ee89ef34d3fd4dfc26
Vansh <vanshchoudhary2744@gmail.com>
2026-09-16 00:46:47 +0530

    web request using fetch

--- dist/webRequest.js
+++ dist/webRequest.js
@@ -1,18 +1,33 @@
 import axios, {} from "axios";
+// const getUser = async () => {
+//   try {
+//     const res: AxiosResponse<User> = await axios.get(
+//       "https://jsonplaceholder.typicode.com/users/1",
+//     );
+//     console.log(res.data);
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       console.error(error.message);
+//     }
+//     if (axios.isAxiosError(error)) {
+//       console.log(error.message);
+//       if (error.response) {
+//         console.log(error.response.status);
+//       }
+//     }
+//   }
+// };
+// getUser();
+//------------------------------
 const getUser = async () => {
     try {
-        const res = await axios.get("https://jsonplaceholder.typicode.com/users/1");
-        console.log(res.data);
+        const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+        const result = await res.json();
+        console.log(result);
     }
     catch (error) {
         if (error instanceof Error) {
-            console.error(error.message);
-        }
-        if (axios.isAxiosError(error)) {
             console.log(error.message);
-            if (error.response) {
-                console.log(error.response.status);
-            }
         }
     }
 };
--- src/webRequest.ts
+++ src/webRequest.ts
@@ -1,29 +1,5 @@
 import axios, { type AxiosResponse } from "axios";
 
-// {
-//   "id": 1,
-//   "name": "Leanne Graham",
-//   "username": "Bret",
-//   "email": "Sincere@april.biz",
-//   "address": {
-//     "street": "Kulas Light",
-//     "suite": "Apt. 556",
-//     "city": "Gwenborough",
-//     "zipcode": "92998-3874",
-//     "geo": {
-//       "lat": "-37.3159",
-//       "lng": "81.1496"
-//     }
-//   },
-//   "phone": "1-770-736-8031 x56442",
-//   "website": "hildegard.org",
-//   "company": {
-//     "name": "Romaguera-Crona",
-//     "catchPhrase": "Multi-layered client-server neural-net",
-//     "bs": "harness real-time e-markets"
-//   }
-// }
-
 interface Address {
   street: string;
   suite: string;
@@ -50,21 +26,37 @@ interface User {
   };
 }
 
+// const getUser = async () => {
+//   try {
+//     const res: AxiosResponse<User> = await axios.get(
+//       "https://jsonplaceholder.typicode.com/users/1",
+//     );
+//     console.log(res.data);
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       console.error(error.message);
+//     }
+//     if (axios.isAxiosError(error)) {
+//       console.log(error.message);
+//       if (error.response) {
+//         console.log(error.response.status);
+//       }
+//     }
+//   }
+// };
+
+// getUser();
+
+//------------------------------
+
 const getUser = async () => {
   try {
-    const res: AxiosResponse<User> = await axios.get(
-      "https://jsonplaceholder.typicode.com/users/1",
-    );
-    console.log(res.data);
+    const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+    const result: User = await res.json();
+    console.log(result);
   } catch (error) {
     if (error instanceof Error) {
-      console.error(error.message);
-    }
-    if (axios.isAxiosError(error)) {
       console.log(error.message);
-      if (error.response) {
-        console.log(error.response.status);
-      }
     }
   }
 };
